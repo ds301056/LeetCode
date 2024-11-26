86caf37eb01147cd987cd15d70b9a19a5ccd0c86
Derek Singleton <ds301056@gmail.com>
2024-11-26 16:31:40 +0000

    Add LeetCode solution with tests

---
+++ C#/1768. Merge Strings Alternately/1768. ImprovedEfficeny.cs
@@ -0,0 +1,18 @@
+csharp
+public class Solution {
+    public string MergeAlternately(string word1, string word2) {
+        var builder = new StringBuilder();
+        int len1 = word1.Length;
+        int len2 = word2.Length;
+        int maxLength = Math.Max(len1, len2);
+
+        for (int i = 0; i < maxLength; i++) {
+            if (i < len1)
+                builder.Append(word1[i]);
+            if (i < len2)
+                builder.Append(word2[i]);
+        }
+
+        return builder.ToString();
+    }
+}
